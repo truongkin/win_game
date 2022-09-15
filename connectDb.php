97a497c15c0a0340463c69698d125b5bbf789792
truongkin189 <truongkin189@gmail.com>
2022-09-15 23:17:32 +0700

    123

--- connectDb.php
+++ connectDb.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('mysql', 'win_game', 'WinGame@123', 'win_game');
+$connect = mysqli_connect('127.0.0.1', 'win_game', 'WinGame@123', 'win_game');
 mysqli_set_charset($connect, "utf8");
 
 // $connect = mysqli_connect('sql101.byethost11.com', 'b11_32292469', '123123', 'b11_32292469_win_game');
