51e4a709301afceda240815239fad2316071d45a
truongkin189 <truongkin189@gmail.com>
2022-09-15 23:13:42 +0700

    update

--- connectDb.php
+++ connectDb.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('mariadb', 'root', 'root', 'game_win');
+$connect = mysqli_connect('mysql', 'win_game', 'WinGame@123', 'win_game');
 mysqli_set_charset($connect, "utf8");
 
 // $connect = mysqli_connect('sql101.byethost11.com', 'b11_32292469', '123123', 'b11_32292469_win_game');
