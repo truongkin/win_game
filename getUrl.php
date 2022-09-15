daefbcb00fa95d68bcfe9a6e5bebe033327b32e8
truongkin189 <truongkin189@gmail.com>
2022-09-16 00:07:32 +0700

    update

--- getUrl.php
+++ getUrl.php
@@ -4,10 +4,14 @@
   $rows = mysqli_query($connect,"
     select * from tb_url
   ");
+  //$data = mysqli_fetch_assoc($rows);
   $data = mysqli_fetch_assoc($rows);
+    $data2 = mysqli_fetch_assoc($rows);
+    $data3 = [];
+    array_push($data3, $data, $data2);
 
   if (count($data) > 0) {
-    echo json_encode($data);
+    echo json_encode($data3);
     } else {
         echo "Fail";
     }
